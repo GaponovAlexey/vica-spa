a8034958afd8af25d4aad91864be3f632fd5b2b3
Kekelberi <alexei.gaponov@gmail.com>
2022-05-24 20:59:58 +0300

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
   },
   plugins: [],
 }
