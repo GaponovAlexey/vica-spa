93645d5167720c9c0f7d96383e04001a59d239d8
Kekelberi <alexei.gaponov@gmail.com>
2022-06-06 14:24:23 +0300

    save

--- src/components/utils/Tog.tsx
+++ src/components/utils/Tog.tsx
@@ -1,32 +1,19 @@
-import { Switch } from '@headlessui/react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
 import s from '../../../styles/css/Home.module.css'
 export const Tog = () => {
-  const [enabled, setEnabled] = useState(false)
-  const router = useRouter()
-
-  const toggle = () => {
-    setEnabled(!enabled)
-    router.locale === 'ru' ? router.back() : router.push('/ru')
-  }
+  const { asPath, locales } = useRouter()
 
   return (
     <div className={s.toggle}>
-      {enabled ? <div>rus</div> : <div>eng</div>}
-      <Switch
-        checked={enabled}
-        onChange={toggle}
-        className={`${enabled ? 'bg-sky-300' : 'bg-sky-700'}
-      relative inline-flex h-[18px] w-[54px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-      >
-        <div className='sr-only'>Use setting</div>
-        <div
-          aria-hidden='true'
-          className={`${enabled ? 'translate-x-9' : 'translate-x-0'}
-        pointer-events-none inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
-      </Switch>
+      {locales &&
+        locales?.map((locale) => (
+          <li key={locale}>
+            <Link href={asPath} locale={locale}>
+              <a>{locale}</a>
+            </Link>
+          </li>
+        ))}
     </div>
   )
 }
