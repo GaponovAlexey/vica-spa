4ef0f40d7183c5a04c68fbed5aaa652fb6181e81
Kekelberi <alexei.gaponov@gmail.com>
2022-06-06 17:26:41 +0300

    save

--- src/components/SKILLS/Skills.tsx
+++ src/components/SKILLS/Skills.tsx
@@ -1,23 +1,46 @@
 import s from '../../../styles/css/Home.module.css'
 import { CustomIMAGE } from '../utils/CustomIMAGE'
-export const Skills = () => {
+export const Skills = (size = 'md') => {
+  let scale = 350
+  if (size === 'sm') scale = 200
+  if (size === 'md') scale = 500
   return (
     <div className={`${s.layout} ${s.skills}`}>
       <h1>Что можно сделать</h1>
       <div>
         <div className=' '>
-          <div className=' p-4'>
+          <div className='p-4'>
             <CustomIMAGE
               src='/img/2.jpg'
-              style={'object-cover object-[0_95%] '}
-              width={350}
-              height={400}
+              style={'object-cover object-[0_45%] '}
+              width={500}
+              height={350}
             />
           </div>
           <h2>Масаж</h2>
         </div>
-        <div>Педикюр</div>
-        <div>Косметология</div>
+        <div>
+          <div className='p-4'>
+            <CustomIMAGE
+              src='/img/2.jpg'
+              style={'object-cover object-[0_45%] '}
+              width={500}
+              height={350}
+            />
+          </div>
+          Педикюр
+        </div>
+        <div>
+          <div className='p-4'>
+            <CustomIMAGE
+              src='/img/2.jpg'
+              style={'object-cover object-[0_45%] '}
+              width={500}
+              height={350}
+            />
+          </div>
+          Косметология
+        </div>
       </div>
     </div>
   )
